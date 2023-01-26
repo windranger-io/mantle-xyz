981d6ee70648d4b23f989f469e7230a6ecdc7967
jasheal <7109973+jasheal@users.noreply.github.com>
2023-01-26 19:43:24 +0000

    build(cms): add directus config (#3)
    * build(cms): add directus config
    
    * test: update testc ci script

---
+++ apps/api-resources/directus.config.js
@@ -0,0 +1,30 @@
+module.exports = function (env) {
+  return {
+    // User inputs from railway starter button
+    ADMIN_EMAIL: env.ADMIN_EMAIL,
+    ADMIN_PASSWORD: env.ADMIN_PASSWORD,
+    KEY: env.KEY,
+    SECRET: env.SECRET,
+
+    // Reference: https://docs.railway.app/deploy/exposing-your-app
+    PORT: env.PORT,
+
+    // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
+    PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
+
+    // Database variables from Railway PostgreSQL Plugin
+    // Reference: https://docs.railway.app/plugins/postgresql
+    DB_CLIENT: 'pg',
+    DB_HOST: env.PGHOST,
+    DB_PORT: env.PGPORT,
+    DB_DATABASE: env.PGDATABASE,
+    DB_USER: env.PGUSER,
+    DB_PASSWORD: env.PGPASSWORD,
+
+    STORAGE_LOCATIONS: 'cloudinary',
+    STORAGE_CLOUDINARY_CLOUD_NAME: env.STORAGE_CLOUDINARY_CLOUD_NAME,
+    STORAGE_CLOUDINARY_API_KEY: env.STORAGE_CLOUDINARY_API_KEY,
+    STORAGE_CLOUDINARY_API_SECRET: env.STORAGE_CLOUDINARY_API_SECRET,
+    STORAGE_CLOUDINARY_ACCESS_MODE: env.STORAGE_CLOUDINARY_ACCESS_MODE,
+  }
+}
