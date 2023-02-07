21a6dc0c468a1f96ac714c213a2a894bbe7871b7
HagiaSofiya <sofiyashrayber@gmail.com>
2023-02-07 18:31:01 -0500

    chore: add comments

--- packages/ui/src/fonts/ThemeFonts.tsx
+++ packages/ui/src/fonts/ThemeFonts.tsx
@@ -2,6 +2,11 @@ import { type ReactNode } from 'react'
 
 import localFont from '@next/font/local'
 
+/**
+ * TODO
+ * add additional font weight if not variable font
+ */
+
 const GTWalsheimRegular = localFont({
   src: './GT-Walsheim-Regular.woff2',
   variable: '--font-GTWalsheimRegular',
