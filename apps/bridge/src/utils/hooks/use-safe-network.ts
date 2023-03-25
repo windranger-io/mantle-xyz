ab3edec300432346e77316d4c8aa15e8fb0a0204
jasheal <7109973+jasheal@users.noreply.github.com>
2023-03-25 00:02:00 +0000

    fix: fix types

--- apps/bridge/src/utils/hooks/use-safe-network.ts
+++ apps/bridge/src/utils/hooks/use-safe-network.ts
@@ -1,24 +1,28 @@
-import { useNetwork } from 'wagmi';
+import { useNetwork, Chain } from 'wagmi'
 
-const useSafeNetwork = () => {
-  const {
-    chain,
-    ...rest
-  } = useNetwork();
+type ChainType = {
+  chains: Chain[]
+  chain: Chain & {
+    unsupported?: boolean | undefined
+  }
+}
+
+const useSafeNetwork = (): ChainType => {
+  const { chain, ...rest } = useNetwork()
 
   if (chain === undefined) {
-    throw new Error('Something went wrong!');
+    throw new Error('Something went wrong!')
   }
 
   // TODO: improve UX
   if (chain.unsupported) {
-    throw new Error('Unsupported chain!');
+    throw new Error('Unsupported chain!')
   }
 
   return {
     chain,
-    ...rest
-  };
-};
+    ...rest,
+  }
+}
 
-export default useSafeNetwork;
+export default useSafeNetwork
