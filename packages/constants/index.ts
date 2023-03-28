b309e73449b941159f208e107fb63fde8d628393
HagiaSofiya <sofiyashrayber@gmail.com>
2023-03-28 17:05:08 -0400

    feat(constants): update constants link

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -6,13 +6,26 @@ export default {
     MEDIUM_LINK: 'https://medium.com/0xmantle',
     TELEGRAM_LINK: 'https://t.me/mantlenetwork'
   },
-  NAV_LINKS: {
+  NAV_LINKS_RELATIVE: {
     ROLES_LINK: '/jobs',
-    GRANTS_LINK: '/grants'
+    GRANTS_LINK: '/grants',
+    PRIVACY_LINK: '/privacy-policy',
+    TERMS_LINK: '/terms'
+  },
+  NAV_LINKS_ABSOLUTE: {
+    ROLES_LINK: 'https://www.mantle.xyz/jobs',
+    GRANTS_LINK: 'https://www.mantle.xyz/grants',
+    PRIVACY_LINK: 'https://www.mantle.xyz/privacy-policy',
+    TERMS_LINK: 'https://www.mantle.xyz/terms'
   },
   RESOURCE_LINKS: {
     BRIDGE_LINK: 'https://bridge.testnet.mantle.xyz',
     FAUCET_LINK: 'https://faucet.testnet.mantle.xyz',
     EXPLORER_LINK: 'https://explorer.testnet.mantle.xyz',
+    WEBSITE_LINK: 'https://www.mantle.xyz/',
+    BRAND_LINK:'https://www.dropbox.com/scl/fo/8vv0daiepm9fmfufbnwhg/h?dl=0&rlkey=an3ndoj14x5d3rywkd6jfhvmr',
+    GITHUB_LINK: 'https://github.com/mantlenetworkio',
+    DOC_LINK: 'https://docs.mantle.xyz/introducing-mantle/a-gentle-introduction'
+
   }
 }
