d42f4fb41f7cec272f2b9d62b2b17515ec5f242b
HagiaSofiya <sofiyashrayber@gmail.com>
2023-04-19 20:09:31 -0400

    feat: display user

--- apps/faucet/src/components/AuthTwtitter.tsx
+++ apps/faucet/src/components/AuthTwtitter.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React from "react";
-import { signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import useHasTweeted from "@hooks/useHasTweeted";
 import { Button, SimpleCard } from "@mantle/ui";
 import Link from "next/link";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 import { Description, Heading } from "./Headings";
 
 function AuthTwitter({
@@ -18,6 +19,7 @@ function AuthTwitter({
 }) {
   // if is verified then disable ui
   const hasTweeted = useHasTweeted(tweets);
+  const { data: session } = useSession();
   // and display verification checkmark
   // const checkmark = hasTweeted ? (
   //   <>
@@ -29,52 +31,64 @@ function AuthTwitter({
   // );
 
   return (
-    <SimpleCard className="max-w-lg grid gap-4">
-      <Heading numDisplay="1" header="Authenticate with Twitter" />
+    <SimpleCard className="max-w-lg w-full  grid gap-4">
+      <Heading numDisplay="1" header="Make a tweet" />
 
-      <Description>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-        {hasTweeted ? (
-          <>
-            {" - "}
-            <button type="button" className="" onClick={() => signOut()}>
-              [disconnect]
-            </button>
-          </>
-        ) : (
-          <span />
-        )}
-      </Description>
+      {!hasTweeted ? (
+        <Description>
+          To request funds via Twitter, you should make a tweet first
+        </Description>
+      ) : (
+        <Description>
+          <div className="flex gap-4 items-center justify-center">
+            Authenticated: {session?.user.username}{" "}
+            <AiOutlineCheckCircle className="text-status-success block text-lg" />{" "}
+          </div>
+        </Description>
+      )}
+
+      {hasTweeted ? (
+        <div className="flex flex-row justify-center align-baseline ">
+          <Button type="button" variant="ghost" onClick={() => signOut()}>
+            Sign Out
+          </Button>
+        </div>
+      ) : (
+        <span />
+      )}
 
       <div className="flex flex-row gap-4">
-        <Link
-          href="https://twitter.com/intent/tweet?text=To%20%23BuildonMantle%2C%20I%20am%20claiming%20test%20%24BIT%20tokens%20on%20faucet.testnet.mantle.xyz%20for%20%400xMantle%2C%20a%20next%20generation%20high-performance%20modular%20%40Ethereum%20L2%20built%20for%20hyperscaled%20dApps.%0A%0ALearn%20more%3A%20%5Bhttps%3A%2F%2Flinktr.ee%2Fmantle.xyz%5D"
-          target="_blank"
-          rel="noreferrer"
-          passHref
-          className="w-full"
-        >
-          <Button
-            variant="secondary"
-            size="full"
-            disabled={hasTweeted || false}
-            className=""
-          >
-            Send Tweet
-          </Button>
-        </Link>
+        {!hasTweeted ? (
+          <>
+            <Link
+              href="https://twitter.com/intent/tweet?text=To%20%23BuildonMantle%2C%20I%20am%20claiming%20test%20%24BIT%20tokens%20on%20faucet.testnet.mantle.xyz%20for%20%400xMantle%2C%20a%20next%20generation%20high-performance%20modular%20%40Ethereum%20L2%20built%20for%20hyperscaled%20dApps.%0A%0ALearn%20more%3A%20%5Bhttps%3A%2F%2Flinktr.ee%2Fmantle.xyz%5D"
+              target="_blank"
+              rel="noreferrer"
+              passHref
+              className="w-full"
+            >
+              <Button
+                variant="secondary"
+                size="full"
+                disabled={hasTweeted || false}
+                className=""
+              >
+                Send Tweet
+              </Button>
+            </Link>
 
-        <Button
-          variant="primary"
-          size="full"
-          type="button"
-          disabled={hasTweeted || false}
-          className=""
-          onClick={() => signIn("twitter")}
-        >
-          Verify Tweet
-        </Button>
+            <Button
+              variant="primary"
+              size="full"
+              type="button"
+              disabled={hasTweeted || false}
+              className=""
+              onClick={() => signIn("twitter")}
+            >
+              Verify Tweet
+            </Button>
+          </>
+        ) : null}
       </div>
     </SimpleCard>
   );
