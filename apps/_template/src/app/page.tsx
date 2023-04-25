91c0260bb917afb849f38233baf133c8696a2ad4
HagiaSofiya <sofiyashrayber@gmail.com>
2023-04-24 22:46:53 -0400

    chore(template): fix test

--- apps/_template/src/app/page.tsx
+++ apps/_template/src/app/page.tsx
@@ -5,5 +5,5 @@ import { Typography } from '@mantle/ui'
  * @todo Updated with real components and content when ready
  */
 export default function Page() {
-  return <Typography>Todo</Typography>
+  return <Typography>Mantle Template</Typography>
 }
