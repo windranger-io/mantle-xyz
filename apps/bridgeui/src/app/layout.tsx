1ab6f314eb4c9103c0bb6d49b13991d1b205dec5
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-03 16:50:11 -0400

    feat: create seperate repo for temp bridge ui

---
+++ apps/bridgeui/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import './styles/globals.css'
+import { GTWalsheimRegular, GTWalsheimMedium } from '@mantle/ui'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${GTWalsheimRegular.variable} ${GTWalsheimMedium.variable}`}
+    >
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ apps/bridgeui/src/app/page.tsx
@@ -0,0 +1,71 @@
+// Dummy components
+import {
+  Header,
+  Footer,
+  PageWrapper,
+  PageBackroundImage,
+  PageContainer,
+  Typography,
+  Button,
+} from '@mantle/ui'
+// Page components
+
+import CONST from '@mantle/constants'
+import bridgeBG from '../../public/bridge-bg.png'
+
+/**
+ *
+ * @todo Updated with real components and content when ready
+ *
+ */
+
+const NAV_ITEMS = [
+  {
+    name: 'Docs',
+    href: CONST.RESOURCE_LINKS.DOC_LINK || '#',
+    internal: false,
+    active: false,
+  },
+  {
+    name: 'Faucet',
+    href: CONST.RESOURCE_LINKS.FAUCET_LINK || '#',
+    internal: false,
+    active: false,
+  },
+  {
+    name: 'Bridge',
+    href: CONST.RESOURCE_LINKS.BRIDGE_LINK || '#',
+    internal: false,
+    active: true,
+  },
+]
+
+export default async function Page() {
+  return (
+    <PageWrapper
+      siteBackroundImage={
+        <PageBackroundImage
+          imgSrc={bridgeBG}
+          altDesc="Faucet Background Image"
+        />
+      }
+      header={
+        <Header
+          navLite
+          walletConnect={<Button>WALLET CONNECT???</Button>}
+          navItems={NAV_ITEMS}
+        />
+      }
+    >
+      <PageContainer className="gap-8">
+        <Typography variant="appPageHeading" className="text-center">
+          Testnet Faucet
+        </Typography>
+
+        <div>
+          <Footer />
+        </div>
+      </PageContainer>
+    </PageWrapper>
+  )
+}
