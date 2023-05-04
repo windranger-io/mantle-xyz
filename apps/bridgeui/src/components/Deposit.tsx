98ebc5739904f9d165b5d10aae283728bf1fa089
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-04 17:53:18 -0400

    feat: direction label (#27)

--- apps/bridgeui/src/components/Deposit.tsx
+++ apps/bridgeui/src/components/Deposit.tsx
@@ -5,9 +5,9 @@ import TransactionPanel from './TransactionPanel'
 export default function Deposit() {
   return (
     <div>
-      Deposit Body
       <TokenSelect />
       <Divider />
+
       <TransactionPanel />
     </div>
   )
