9a59e4a72a9fb1066c33a03070632d75cac61a9f
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-04 18:07:59 -0400

    feat: destination display (#28)

--- apps/bridgeui/src/components/Withdraw.tsx
+++ apps/bridgeui/src/components/Withdraw.tsx
@@ -1,3 +1,4 @@
+import Destination from './Destination'
 import Divider from './Divider'
 import TokenSelect from './TokenSelect'
 import TransactionPanel from './TransactionPanel'
@@ -7,6 +8,7 @@ export default function Withdraw() {
     <div>
       <TokenSelect />
       <Divider />
+      <Destination />
       <TransactionPanel />
     </div>
   )
