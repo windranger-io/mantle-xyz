530759710d8adef79c5331043abc94773acb05b2
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-04 17:53:18 -0400

    feat: direction label (#27)

---
+++ apps/bridgeui/src/components/DirectionLabel.tsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mantle/ui'
+
+export default function DirectionLabel({
+  direction,
+  logo,
+  chain,
+}: {
+  direction: string
+  logo: React.ReactNode
+  chain: string
+}) {
+  return (
+    <div className="flex space-x-2.5">
+      <Typography variant="body">{direction}:</Typography>
+      <div className="flex space-x-2">
+        {logo}
+        <Typography variant="body"> {chain}</Typography>
+      </div>
+    </div>
+  )
+}
