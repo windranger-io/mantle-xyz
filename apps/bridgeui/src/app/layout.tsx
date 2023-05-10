f09dcb2a6fb13db975b50186fc227d3ee1263e81
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-10 18:59:54 -0400

    chore: move page layout into layout

--- apps/bridgeui/src/app/layout.tsx
+++ apps/bridgeui/src/app/layout.tsx
@@ -1,8 +1,42 @@
 import "./styles/globals.css";
 
-import { GTWalsheimRegular, GTWalsheimMedium } from "@mantle/ui";
+// Dummy components
+import {
+  GTWalsheimRegular,
+  GTWalsheimMedium,
+  Header,
+  Footer,
+  PageWrapper,
+  PageBackroundImage,
+  PageContainer,
+  Typography,
+} from "@mantle/ui";
 
+import CONST from "@mantle/constants";
+import ConnectWallet from "@components/ConnectWallet";
+import bridgeBG from "../../public/bridge-bg.png";
 import Providers from "./providers";
+
+const NAV_ITEMS = [
+  {
+    name: "Docs",
+    href: CONST.RESOURCE_LINKS.DOC_LINK || "#",
+    internal: false,
+    active: false,
+  },
+  {
+    name: "Faucet",
+    href: CONST.RESOURCE_LINKS.FAUCET_LINK || "#",
+    internal: false,
+    active: false,
+  },
+  {
+    name: "Bridge",
+    href: CONST.RESOURCE_LINKS.BRIDGE_LINK || "#",
+    internal: false,
+    active: true,
+  },
+];
 
 export default function RootLayout({
   children,
@@ -16,7 +50,33 @@ export default function RootLayout({
     >
       <body>
         {/* @ts-expect-error Server Component */}
-        <Providers>{children}</Providers>
+        <Providers>
+          <PageWrapper
+            siteBackroundImage={
+              <PageBackroundImage
+                imgSrc={bridgeBG}
+                altDesc="Bridge Background Image"
+              />
+            }
+            header={
+              <Header
+                navLite
+                walletConnect={<ConnectWallet />}
+                navItems={NAV_ITEMS}
+              />
+            }
+          >
+            <PageContainer className="gap-8">
+              <Typography variant="appPageHeading" className="text-center">
+                Testnet Bridge
+              </Typography>
+              {children}
+              <div>
+                <Footer />
+              </div>
+            </PageContainer>
+          </PageWrapper>
+        </Providers>
       </body>
     </html>
   );
