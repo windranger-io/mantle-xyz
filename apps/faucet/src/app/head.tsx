0a689db9e9671844444d4362411d17d9e0fbe415
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-10 12:10:09 -0400

    chore: update favicon (#34)

--- apps/faucet/src/app/head.tsx
+++ apps/faucet/src/app/head.tsx
@@ -1,4 +1,11 @@
-import { APP_NAME, META, OG_TITLE, TWITTER_DESC } from "@config/constants";
+import {
+  ABSOLUTE_PATH,
+  APP_NAME,
+  META,
+  OG_TITLE,
+  TWITTER_DESC,
+  TWITTER_TITLE,
+} from "@config/constants";
 
 export default function Head() {
   return (
@@ -8,21 +15,15 @@ export default function Head() {
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={`${OG_TITLE}`} />
-      <meta
-        property="og:image"
-        content="https://mantle-prod.vercel.app/preview.jpg"
-      />
+      <meta property="og:image" content={`${ABSOLUTE_PATH}/opengraph.png`} />
       <meta name="description" content={`${META}`} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@0xMantle" />
-      <meta name="twitter:title" content={`${APP_NAME}`} />
+      <meta name="twitter:title" content={`${TWITTER_TITLE}`} />
       <meta name="twitter:creator" content="@0xMantle" />
       <meta name="twitter:description" content={`${TWITTER_DESC}`} />
-      <meta
-        name="twitter:image:src"
-        content="https://mantle-prod.vercel.app/preview.jpg"
-      />
+      <meta name="twitter:image:src" content={`${ABSOLUTE_PATH}/twitter.png`} />
       <meta name="google" content="nositelinkssearchbox" />
     </>
   );
