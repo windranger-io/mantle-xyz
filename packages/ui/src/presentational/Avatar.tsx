53471c9c415d1c10be851abe075da198f564a038
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-12 18:43:10 -0400

    feat(faucet): transaction page accountd details

--- packages/ui/src/presentational/Avatar.tsx
+++ packages/ui/src/presentational/Avatar.tsx
@@ -3,12 +3,13 @@ import BoringAvatar from 'boring-avatars'
 
 export type AvatarProps = {
   walletAddress: string
+  size: number
 }
 
-const Avatar = ({ walletAddress }: AvatarProps) => {
+const Avatar = ({ walletAddress, size = 18 }: AvatarProps) => {
   return (
     <BoringAvatar
-      size={18}
+      size={size}
       name={walletAddress}
       variant="pixel"
       colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
