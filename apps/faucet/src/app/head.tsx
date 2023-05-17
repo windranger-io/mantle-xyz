81cb4e12c910c54b5516685f6b0b783bf546f5af
HagiaSofiya <sofiyashrayber@gmail.com>
2023-05-17 15:05:43 -0400

    feat: add viewport metatag to trigger

--- apps/faucet/src/app/head.tsx
+++ apps/faucet/src/app/head.tsx
@@ -12,6 +12,7 @@ export default function Head() {
     <>
       <title>{APP_NAME}</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={`${OG_TITLE}`} />
