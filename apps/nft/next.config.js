eac9d0a1199097c9e7c96a3d6aceb39d37d58ead
HagiaSofiya <sofiyashrayber@gmail.com>
2023-06-14 21:45:56 -0400

    chore: update next config

--- apps/nft/next.config.js
+++ apps/nft/next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const withNextIntl = require('next-intl/plugin')('./src/i18n.ts')
-const nextConfig = {
+
+const nextConfig = withNextIntl({
   reactStrictMode: true,
   generateEtags: false,
   transpilePackages: ['@design-system/ui'],
   experimental: {
     appDir: true,
   },
-}
+})
 
 module.exports = nextConfig
