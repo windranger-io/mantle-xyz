d7af95a728667d030aec5fbfe2471612d437d3e8
octavioamu <octavioamuchastegui@gmail.com>
2023-06-19 21:13:27 -0300

    fix: meta description and whitelist og images

--- apps/nft/src/config/constants.ts
+++ apps/nft/src/config/constants.ts
@@ -4,14 +4,14 @@ export const APP_NAME =
 
 // Configure deta description
 export const META =
-  "A new journey awaits. Enter into an ecosystem filled with never ending rainforest, rich minerals and gemstones, and adventure around every corner. ";
+  "A new journey awaits. Enter into an ecosystem filled with never ending rainforest, rich minerals and gemstones, and adventure around every corner.";
 
 // Configure OG Title
 export const OG_TITLE = "The Gateway to Mantle Will Open Soon";
 
 // Configure OG Desc
 export const OG_DESC =
-  "Get ready to enter into Mantle's adventure filled ecosystem.";
+  "Get ready to enter Mantle's adventure-filled ecosystem.";
 
 // Google Tag
 export const GOOGLE_TAG = "GTM-PX2HN6L";
