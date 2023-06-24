ac4673ade25c839bfbdf52d3aef6cd3a3fb9ac1c
grezle <4310551+grezle@users.noreply.github.com>
2023-06-24 03:45:36 +0100

    fix(bridge): make sure balance formatting always displays all digits

--- apps/bridge/src/utils/formatStrings.ts
+++ apps/bridge/src/utils/formatStrings.ts
@@ -1,33 +1,46 @@
+"use client";
+
 import Decimal from "decimal.js";
+
+export const localeZero = new Intl.NumberFormat(
+  globalThis.navigator?.language || "en",
+  {
+    minimumFractionDigits: 1,
+  }
+).format(Number("0.0"));
 
 export const formatBigNumberString = (
   str: string,
   precision = 6,
-  locale = "en"
+  groupingInt = true,
+  groupingFrac = true,
+  locale = globalThis.navigator?.language || "en"
 ) => {
-  let integerPart = BigInt(str.split(".")[0] || 0);
-  let fractionPart = new Decimal(`0.${str?.split(".")?.[1] || 0}`)
+  const integerPart = BigInt(str.split(".")[0] || 0);
+  const fractionPart = new Decimal(
+    `0.${str?.split(".")?.[1]?.slice(0, precision) || 0}`
+  )
     .toFixed(precision)
     .toString()
     .replace(/^0|0+$/g, "")
     .replace(/^([^0-9])$/, "$10");
 
-  // if the fraction part rolls over we need to apply to integerPart
-  if (parseFloat(fractionPart) >= 1) {
-    integerPart += BigInt(1);
-    fractionPart = fractionPart.replace(/^1/, "");
-  }
-
   // format according to the given locale
-  const mainFormat = new Intl.NumberFormat(locale, {
+  const mainFormatInt = new Intl.NumberFormat(locale, {
     minimumFractionDigits: 0,
+    useGrouping: groupingInt,
+  });
+  // format according to the given locale
+  const mainFormatFrac = new Intl.NumberFormat(locale, {
+    minimumFractionDigits: 0,
+    useGrouping: groupingFrac,
   });
 
   // construct a template to pull locales decimal seperator
-  const template = mainFormat.format(1.1);
+  const template = mainFormatInt.format(1.1);
 
   // reconnect the number parts and place templates seperator
-  return `${mainFormat.format(integerPart)}${template[1]}${mainFormat
+  return `${mainFormatInt.format(integerPart)}${template[1]}${mainFormatFrac
     .format(
       parseInt(
         (fractionPart.length === 1 ? `${fractionPart}0` : fractionPart)
