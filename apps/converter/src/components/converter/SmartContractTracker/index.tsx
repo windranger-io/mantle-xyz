430872b085fd14a079e798c1647c98e13ad3bec6
Abby L <yu950713@gmail.com>
2023-07-05 21:07:25 +0800

    fix: converter - sc hydration issue again

--- apps/converter/src/components/converter/SmartContractTracker/index.tsx
+++ apps/converter/src/components/converter/SmartContractTracker/index.tsx
@@ -1,4 +1,5 @@
 import { useBalance } from "wagmi";
+import { useEffect, useState } from "react";
 import { formatUnits, parseUnits } from "ethers/lib/utils.js";
 import { Typography } from "@mantle/ui";
 import { cn } from "@mantle/ui/src/utils";
@@ -20,8 +21,14 @@ export function SmartContractTracker({ halted }: SCTrackerProps) {
     suspense: true,
   });
 
+  // to avoid hydration error
+  const [balanceDataClient, setBalanceDataClient] = useState<string>("");
+  useEffect(() => {
+    setBalanceDataClient(balanceData?.formatted || "");
+  }, [balanceData?.formatted]);
+
   const formatted = formatUnits(
-    parseUnits(balanceData?.formatted || "0", L1_MANTLE_TOKEN.decimals),
+    parseUnits(balanceDataClient || "0", L1_MANTLE_TOKEN.decimals),
     L1_MANTLE_TOKEN.decimals
   );
 
