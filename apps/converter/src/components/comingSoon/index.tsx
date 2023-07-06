7b1040e7a404c6c3ff292ea77870d799abf5214e
Abby L <yu950713@gmail.com>
2023-07-06 17:36:34 +0800

    feat: converter - add local time to coming soon

--- apps/converter/src/components/comingSoon/index.tsx
+++ apps/converter/src/components/comingSoon/index.tsx
@@ -1,16 +1,22 @@
 import { Typography } from "@mantle/ui";
+import { getLocalDateTime } from "@utils/formatDate";
 
 export default function ComingSoon() {
+  const releaseDate = new Date(Date.UTC(2023, 6, 17, 15, 0, 0));
+
   return (
     <>
       <div className="md:grow md:mt-0 mt-36 flex justify-center items-center">
-        <Typography className="text-center md:text-[80px] text-[42px] font-medium">
+        <Typography className="text-center md:text-[80px] text-[42px] font-medium text-white">
           Migrate $BIT
         </Typography>
       </div>
 
-      <Typography className="md:my-40 my-8 text-center md:text-[42px] text-[28px] font-medium">
-        Coming soon
+      <Typography className="md:mt-40 mt-8 mb-14 text-center md:text-[42px] text-[28px] font-medium text-white">
+        Release date:
+      </Typography>
+      <Typography className="md:mb-14 mb-4 text-center md:text-[32px] text-[28px] font-medium">
+        {getLocalDateTime(releaseDate)}
       </Typography>
     </>
   );
---
+++ apps/converter/src/utils/formatDate.ts
@@ -0,0 +1,25 @@
+const localDateTimeIntl = new Intl.DateTimeFormat(
+  // globalThis.navigator?.language ||
+  "en",
+  {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    hour12: true,
+    timeZoneName: "short",
+  }
+);
+
+export const getLocalDateTime = (date: number | Date | undefined) => {
+  const formattedDateTime = localDateTimeIntl.format(date);
+
+  // Extracting the date and time components
+  const [datePart, timePart] = formattedDateTime.split(",");
+
+  // Transforming the date part into the desired format (YYYY-MM-DD)
+  const [month, day, year] = datePart.split("/");
+  const formattedDate = `${year}-${month}-${day}`;
+
+  return `${formattedDate} ${timePart}`;
+};
