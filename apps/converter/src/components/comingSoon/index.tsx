49551fff8a8862cb51fdadd28ea11c9eb07f3aec
Abby L <yu950713@gmail.com>
2023-07-06 20:27:31 +0800

    feat: converter - update copy

--- apps/converter/src/components/comingSoon/index.tsx
+++ apps/converter/src/components/comingSoon/index.tsx
@@ -5,7 +5,7 @@ export default function ComingSoon() {
     <>
       <div className="md:grow md:mt-0 mt-36 flex justify-center items-center">
         <Typography className="text-center md:text-[80px] text-[42px] font-medium">
-          Migrate $BIT
+          Migrator
         </Typography>
       </div>
 
