b6fa24f7fb6d2252d1b5cbe4c6ace73f70373113
oanskyy <oana.anghelachi@gmail.com>
2023-07-12 12:19:34 -0700

    feat(bridge): add legal disclaimer component for mainnet

---
+++ apps/bridge/src/components/LegalDisclaimer.tsx
@@ -0,0 +1,11 @@
+import { MantleLetterMarkBridge } from "@mantle/ui";
+
+function LegalDisclaimer() {
+  return (
+    <div className="pb-8 md:pb-20 px-4 md:px-9">
+      <MantleLetterMarkBridge />
+    </div>
+  );
+}
+
+export default LegalDisclaimer;
