f0088906acdb40d26fa0c295b5368a92f17e50a0
oanskyy <oana.anghelachi@gmail.com>
2023-07-12 14:43:34 -0500

    trigger preview

--- apps/bridge/src/components/LegalDisclaimer.tsx
+++ apps/bridge/src/components/LegalDisclaimer.tsx
@@ -2,7 +2,7 @@ import { MantleLetterMarkBridge } from "@mantle/ui";
 
 function LegalDisclaimer() {
   return (
-    <div className="pb-8 md:pb-20 px-4 md:px-9">
+    <div className="pb-8 md:pb-20 px-4 md:px-9 ">
       <MantleLetterMarkBridge />
     </div>
   );
