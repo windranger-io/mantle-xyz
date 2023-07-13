8388a489638891adeaf2e102eb66342534f2b865
Abby L <yu950713@gmail.com>
2023-07-13 14:32:44 +0800

    feat: migrator - setup apollo provider

---
+++ apps/converter/src/providers/apolloProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ApolloClient, ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  // the client should always be pointing at the locally running supagraph endpoint
+  const httpLink = new HttpLink({
+    uri: "/graphql",
+  });
+
+  return new ApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
