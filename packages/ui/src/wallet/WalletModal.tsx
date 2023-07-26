fcdf394e51aadb723835ad2738a644225748b8fc
grezle <4310551+grezle@users.noreply.github.com>
2023-07-26 17:28:21 +0100

    fix(ui): prevent dialog closing when interacting with WC

--- packages/ui/src/wallet/WalletModal.tsx
+++ packages/ui/src/wallet/WalletModal.tsx
@@ -22,8 +22,11 @@ export const WalletModal = ({
   <Dialog.Root>
     <Dialog.Trigger asChild>{children}</Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="bg-black/60 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-[9]" />
-      <Dialog.Content className="bg-black data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[403px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none border border-white/20 px-12 py-14 z-[10]">
+      <Dialog.Overlay className="bg-black/60 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-[1]" />
+      <Dialog.Content
+        className="bg-black data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[403px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none border border-white/20 px-12 py-14 z-[10]"
+        onPointerDownOutside={event => event.preventDefault()}
+      >
         <div className="relative">
           <p className="text-2xl pb-6 mb-6 border-b border-white/20">
             Connect your wallet
