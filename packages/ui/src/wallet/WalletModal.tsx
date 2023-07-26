91d59e09e39b8e06da0e99a25a62b9d5f887630f
grezle <4310551+grezle@users.noreply.github.com>
2023-07-26 09:38:38 +0100

    feat(ui): introduce walletConnect option in ui

--- packages/ui/src/wallet/WalletModal.tsx
+++ packages/ui/src/wallet/WalletModal.tsx
@@ -1,23 +1,29 @@
 'use client'
 
-/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/jsx-no-target-blank, react/require-default-props */
 
 import { ReactNode } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { Button } from '../actions/Button'
 import { MetaMaskSvg } from './MetaMask'
+import { WalletConnectSvg } from './WalletConnect'
 
 type WalletModalProps = {
   children: ReactNode
-  onMetamask: () => void
+  onMetamask?: () => void
+  onWalletConnect?: () => void
 }
 
-export const WalletModal = ({ children, onMetamask }: WalletModalProps) => (
+export const WalletModal = ({
+  children,
+  onMetamask = undefined,
+  onWalletConnect = undefined,
+}: WalletModalProps) => (
   <Dialog.Root>
     <Dialog.Trigger asChild>{children}</Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="bg-black/60 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-[999]" />
-      <Dialog.Content className="bg-black data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[403px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none border border-white/20 px-12 py-14 z-[1000]">
+      <Dialog.Overlay className="bg-black/60 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-[9]" />
+      <Dialog.Content className="bg-black data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[403px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none border border-white/20 px-12 py-14 z-[10]">
         <div className="relative">
           <p className="text-2xl pb-6 mb-6 border-b border-white/20">
             Connect your wallet
@@ -41,16 +47,30 @@ export const WalletModal = ({ children, onMetamask }: WalletModalProps) => (
             </Dialog.Close>
           </div>
         </div>
-        <Button
-          type="button"
-          variant="secondary"
-          size="full"
-          onClick={onMetamask}
-          className="flex flex-row items-center justify-center text-base min-h-[48px] text-white gap-2 backdrop-blur-[50px] bg-white/10 hover:bg-white/20 cursor-pointer w-full"
-        >
-          <MetaMaskSvg className="h-6 w-6" />
-          Metamask
-        </Button>
+        {onMetamask && (
+          <Button
+            type="button"
+            variant="secondary"
+            size="full"
+            onClick={onMetamask}
+            className="flex flex-row items-center justify-center text-base min-h-[48px] text-white gap-2 backdrop-blur-[50px] bg-white/10 hover:bg-white/20 cursor-pointer w-full my-2"
+          >
+            <MetaMaskSvg className="h-6 w-6" />
+            Metamask
+          </Button>
+        )}
+        {onWalletConnect && (
+          <Button
+            type="button"
+            variant="secondary"
+            size="full"
+            onClick={onWalletConnect}
+            className="flex flex-row items-center justify-center text-base min-h-[48px] text-white gap-2 backdrop-blur-[50px] bg-white/10 hover:bg-white/20 cursor-pointer w-full my-2"
+          >
+            <WalletConnectSvg className="h-6 w-6" />
+            Wallet Connect
+          </Button>
+        )}
         <p className="text-sm mt-6">
           By connecting your wallet, you hereby acknowledge that you have read
           and accept the{' '}
