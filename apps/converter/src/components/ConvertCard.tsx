97e7a446eef923d5164565fbeaceb6b9f0360dbc
abbylow <yu950713@gmail.com>
2023-07-28 15:55:58 +0800

    Scr 214 fix padding between migrator and faq (#139)
    * fix(migrator): form max width 484px
    
    * fix(migrator): space between migrator and faq

--- apps/converter/src/components/ConvertCard.tsx
+++ apps/converter/src/components/ConvertCard.tsx
@@ -11,7 +11,7 @@ export function ConvertCard({ children, className }: Props) {
   return (
     <div
       className={cn(
-        "md:max-w-lg w-full grid gap-4 relative bg-[#000000] overflow-y-auto overflow-x-clip md:overflow-hidden border border-[#1C1E20] rounded-t-[30px] rounded-b-[20px] mx-auto",
+        "max-w-[484px] w-full grid gap-4 relative bg-[#000000] overflow-y-auto overflow-x-clip md:overflow-hidden border border-[#1C1E20] rounded-t-[30px] rounded-b-[20px] mx-auto",
         className
       )}
     >
