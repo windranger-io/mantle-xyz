dea974f5f56d3ebaef8a39b1dd40ab314a901127
Abby L <yu950713@gmail.com>
2023-08-03 18:04:28 +0800

    fix(website): update header props

---
+++ apps/website/src/components/Nav.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Button } from '@mantle/ui'
+import { Header } from '@mantle/ui/src/navigation/Header'
+
+type NavProps = {
+  // eslint-disable-next-line react/require-default-props
+  className?: string | undefined
+}
+
+function Nav({ className = '' }: NavProps) {
+  return (
+    <Header
+      walletConnect={<Button>Wallet Connect???</Button>}
+      className={className}
+      isTestnet
+      activeKey="website"
+      mobileMenuOpen={false}
+      setMobileMenuOpen={() => {}}
+    />
+  )
+}
+
+export default Nav
