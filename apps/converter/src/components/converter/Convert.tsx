c1db5c2ea3b6e27ab273a994b4f62b47da7e2e90
Abby L <yu950713@gmail.com>
2023-08-15 16:06:38 +0800

    feat: research walletconnect web3modal

--- apps/converter/src/components/converter/Convert.tsx
+++ apps/converter/src/components/converter/Convert.tsx
@@ -1,83 +1,90 @@
 "use client";
 
-import {
-  Views,
-  L1_CONVERTER_CONTRACT_ABI,
-  L1_CONVERTER_CONTRACT_ADDRESS,
-} from "@config/constants";
-import StateContext from "@providers/stateContext";
-import { Suspense, useContext, useEffect, useState } from "react";
-import { Contract } from "ethers";
+// import {
+//   Views,
+//   L1_CONVERTER_CONTRACT_ABI,
+//   L1_CONVERTER_CONTRACT_ADDRESS,
+// } from "@config/constants";
+// import StateContext from "@providers/stateContext";
+// import {
+// Suspense,
+// useContext,
+// useEffect,
+// useState,
+// } from "react";
+// import { Contract } from "ethers";
+import { Web3Button } from "@web3modal/react";
 
 // by order of use...
-import Dialogue from "@components/converter/dialogue";
-import From from "@components/converter/From";
-import Hr from "@components/converter/Divider";
-import To from "@components/converter/To";
-import CTA from "@components/converter/CTA";
-import ErrorMsg from "@components/converter/ErrorMsg";
-import TX from "@components/converter/TransactionPanel";
-import { Typography } from "@mantle/ui";
-import { ConvertCard } from "@components/ConvertCard";
-import { SmartContractTracker } from "./SmartContractTracker";
-import { Loading as SCLoading } from "./SmartContractTracker/Loading";
-import { Faq } from "./Faq";
+// import Dialogue from "@components/converter/dialogue";
+// import From from "@components/converter/From";
+// import Hr from "@components/converter/Divider";
+// import To from "@components/converter/To";
+// import CTA from "@components/converter/CTA";
+// import ErrorMsg from "@components/converter/ErrorMsg";
+// import TX from "@components/converter/TransactionPanel";
+// import { Typography } from "@mantle/ui";
+// import { ConvertCard } from "@components/ConvertCard";
+// import { SmartContractTracker } from "./SmartContractTracker";
+// import { Loading as SCLoading } from "./SmartContractTracker/Loading";
+// import { Faq } from "./Faq";
 
 export default function Convert() {
   // unpack the context
-  const { provider, view, isCTAPageOpen, setIsCTAPageOpen } =
-    useContext(StateContext);
+  // const { provider, view, isCTAPageOpen, setIsCTAPageOpen } =
+  //   useContext(StateContext);
 
-  const [halted, setHalted] = useState<boolean>(true);
+  // const [halted, setHalted] = useState<boolean>(true);
 
-  useEffect(() => {
-    // read halted from contract
-    const getHaltedStatus = async () => {
-      const contract = new Contract(
-        L1_CONVERTER_CONTRACT_ADDRESS,
-        L1_CONVERTER_CONTRACT_ABI,
-        provider
-      );
-      const status = await contract.halted();
-      setHalted(status);
-    };
-    getHaltedStatus();
-  }, [provider]);
+  // useEffect(() => {
+  //   // read halted from contract
+  //   const getHaltedStatus = async () => {
+  //     const contract = new Contract(
+  //       L1_CONVERTER_CONTRACT_ADDRESS,
+  //       L1_CONVERTER_CONTRACT_ABI,
+  //       provider
+  //     );
+  //     const status = await contract.halted();
+  //     setHalted(status);
+  //   };
+  //   getHaltedStatus();
+  // }, [provider]);
 
-  if (view !== Views.Default) {
-    return <span />;
-  }
+  // if (view !== Views.Default) {
+  //   return <span />;
+  // }
 
-  if (isCTAPageOpen) {
-    return <Dialogue />;
-  }
+  // if (isCTAPageOpen) {
+  //   return <Dialogue />;
+  // }
 
   return (
-    <div className="md:px-8 w-full">
-      <Typography variant="appPageHeading" className="text-center text-[42px]">
-        Migrator
-      </Typography>
-      <Typography variant="body" className="text-center mt-6 mb-2">
-        The migration is irreversible.
-      </Typography>
-      <div className="lg:relative lg:w-[484px] flex flex-col md:flex-row md:justify-center md:items-start md:gap-5 gap-4 lg:mx-auto ">
-        <ConvertCard className="min-w-[320px]">
-          <From />
-          <Hr />
-          <To />
-          <div className="px-5 pb-4">
-            <CTA setIsOpen={setIsCTAPageOpen} halted={!!halted} />
-            <ErrorMsg halted={!!halted} />
-            <TX />
-          </div>
-        </ConvertCard>
-        <div className="flex flex-col xl:w-[280px] lg:w-[240px] md:max-w-[328px] md:min-w-[250px] lg:absolute lg:top-0 lg:-right-[260px] xl:-right-[396px] w-auto">
-          <Suspense fallback={<SCLoading />}>
-            <SmartContractTracker halted={!!halted} />
-          </Suspense>
-          <Faq />
-        </div>
-      </div>
-    </div>
+    // <div className="md:px-8 w-full">
+    //   <Typography variant="appPageHeading" className="text-center text-[42px]">
+    //     Migrator
+    //   </Typography>
+    //   <Typography variant="body" className="text-center mt-6 mb-2">
+    //     The migration is irreversible.
+    //   </Typography>
+    //   <div className="lg:relative lg:w-[484px] flex flex-col md:flex-row md:justify-center md:items-start md:gap-5 gap-4 lg:mx-auto ">
+    //     <ConvertCard className="min-w-[320px]">
+    //       <From />
+    //       <Hr />
+    //       <To />
+    //       <div className="px-5 pb-4">
+    //         <CTA setIsOpen={setIsCTAPageOpen} halted={!!halted} />
+    //         <ErrorMsg halted={!!halted} />
+    //         <TX />
+    //       </div>
+    //     </ConvertCard>
+    //     <div className="flex flex-col xl:w-[280px] lg:w-[240px] md:max-w-[328px] md:min-w-[250px] lg:absolute lg:top-0 lg:-right-[260px] xl:-right-[396px] w-auto">
+    //       <Suspense fallback={<SCLoading />}>
+    //         <SmartContractTracker halted={!!halted} />
+    //       </Suspense>
+    //       <Faq />
+    //     </div>
+    //   </div>
+    // </div>
+    <Web3Button />
   );
 }
--- apps/converter/src/providers/wagmiContext.tsx
+++ apps/converter/src/providers/wagmiContext.tsx
@@ -5,52 +5,70 @@ import { CHAINS_FORMATTED, L1_CHAIN_ID } from "@config/constants";
 
 // Required components for wagmi...
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { Web3Modal } from "@web3modal/react";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
 
-import { InjectedConnector } from "wagmi/connectors/injected";
+// import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+// import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-import { publicProvider } from "wagmi/providers/public";
+// import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+// import { publicProvider } from "wagmi/providers/public";
+
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID || "";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   // We support L1 and Mantle (depending on state of ui)
   [CHAINS_FORMATTED[L1_CHAIN_ID]],
-  [
-    jsonRpcProvider({
-      rpc: (chain) => ({
-        http: chain.rpcUrls.default.http[0],
-      }),
-    }),
-    publicProvider(),
-  ]
+  // [
+  //   jsonRpcProvider({
+  //     rpc: (chain) => ({
+  //       http: chain.rpcUrls.default.http[0],
+  //     }),
+  //   }),
+  //   publicProvider(),
+  // ]
+  [w3mProvider({ projectId })]
 );
 
 const config = createConfig({
   autoConnect: true,
+  // connectors: [
+  //   new MetaMaskConnector({
+  //     chains,
+  //     options: {
+  //       UNSTABLE_shimOnConnectSelectAccount: true,
+  //     },
+  //   }),
+  //   new InjectedConnector({
+  //     chains,
+  //     options: {
+  //       name: (detectedName) =>
+  //         `Injected (${
+  //           typeof detectedName === "string"
+  //             ? detectedName
+  //             : detectedName.join(", ")
+  //         })`,
+  //       shimDisconnect: true,
+  //     },
+  //   }),
+  //   new WalletConnectConnector({
+  //     chains,
+  //     options: {
+  //       projectId: projectId,
+  //     },
+  //   }),
+  // ],
   connectors: [
+    ...w3mConnectors({ projectId, chains }),
     new MetaMaskConnector({
       chains,
       options: {
         UNSTABLE_shimOnConnectSelectAccount: true,
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: (detectedName) =>
-          `Injected (${
-            typeof detectedName === "string"
-              ? detectedName
-              : detectedName.join(", ")
-          })`,
-        shimDisconnect: true,
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_ID || "",
       },
     }),
   ],
@@ -58,12 +76,26 @@ const config = createConfig({
   webSocketPublicClient,
 });
 
+const ethereumClient = new EthereumClient(config, chains);
+
 interface WagmiProviderProps {
   children: React.ReactNode;
 }
 
 function WagmiProvider({ children }: WagmiProviderProps) {
-  return <WagmiConfig config={config}>{children}</WagmiConfig>;
+  return (
+    <>
+      <WagmiConfig config={config}>{children}</WagmiConfig>
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        explorerRecommendedWalletIds={[
+          "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369", // rainbow
+          "18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1", // rabby
+        ]}
+      />
+    </>
+  );
 }
 
 export { WagmiProvider };
