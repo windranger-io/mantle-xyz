567ac70c250b29959619a4f41019b26248f67ed0
grezle <4310551+grezle@users.noreply.github.com>
2023-08-26 00:00:14 +0100

    fix(token-graph): disable cors

--- apps/token-graph/next.config.js
+++ apps/token-graph/next.config.js
@@ -19,6 +19,20 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        // matching all routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: "Origin, X-Requested-With, Content-Type, Accept, Authorization" },
+        ]
+      }
+    ]
+  },
   experimental: {
     appDir: true,
     scrollRestoration: true
