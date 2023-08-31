e60174380e9ba4b88168e784c5f0b133c333c467
Abby L <yu950713@gmail.com>
2023-08-31 23:47:52 +0800

    feat(charity): + and - buttons for num of token input

--- apps/charity/src/components/Form.tsx
+++ apps/charity/src/components/Form.tsx
@@ -28,6 +28,8 @@ export default function Form() {
     remainingCopy = remainingNFT;
   }
 
+  const [numOfToken, setNumOfToken] = useState<number>(1);
+
   return (
     <div className="grow sm:my-16 my-10">
       <div className="sm:w-[444px] w-[320px] flex flex-col items-center gap-4 pt-10 pb-5 sm:px-[62px] px-3 rounded-[40px] border border-boxes-containerStroke bg-boxes-containerBg">
@@ -43,9 +45,105 @@ export default function Form() {
         <Typography variant="h5Title" className="sm:hidden block text-center">
           {remainingCopy}
         </Typography>
-        {/* TODO: input */}
+        {/* input */}
         {remainingNFT === null || remainingNFT > 0 ? (
-          <div>input</div>
+          <div className="flex justify-between items-center p-2 rounded-[40px] border border-stroke-inputs bg-boxes-containerBg w-full">
+            <button
+              type="button"
+              disabled={numOfToken <= 1}
+              onClick={() => setNumOfToken((prev) => prev - 1)}
+              className={`${
+                numOfToken <= 1 ? "cursor-not-allowed" : "cursor-pointer"
+              } h-8 w-8`}
+            >
+              {/* minus icon */}
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="16"
+                  cy="16"
+                  r="15.5"
+                  stroke={numOfToken <= 1 ? "#696969" : "white"}
+                />
+                <path
+                  d="M9.5 16.7499V15.25H22.5V16.7499H9.5Z"
+                  fill={numOfToken <= 1 ? "#696969" : "white"}
+                />
+              </svg>
+            </button>
+            {/* <input
+              type="number"
+              required
+              id="numOfToken"
+              value={numOfToken}
+              min={1}
+              max={remainingNFT !== null && remainingNFT < 5 ? remainingNFT : 5}
+              // className="bg-black w-full rounded-input"
+              // disabled={
+              //   minting ||
+              //   !hasTweeted ||
+              //   (balanceMNT && parseFloat(myBalanceMNT) >= MAX_BALANCE)
+              // }
+              // onChange={(e: {
+              //   target: { value: React.SetStateAction<number> };
+              // }) => {
+              //   setNumOfToken(value);
+              // }}
+            /> */}
+            <button
+              type="button"
+              disabled={
+                numOfToken >=
+                (remainingNFT !== null && remainingNFT < 5 ? remainingNFT : 5)
+              }
+              onClick={() => setNumOfToken((prev) => prev + 1)}
+              className={`${
+                numOfToken >=
+                (remainingNFT !== null && remainingNFT < 5 ? remainingNFT : 5)
+                  ? "cursor-not-allowed"
+                  : "cursor-pointer"
+              } h-8 w-8`}
+            >
+              {/* plus icon */}
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="16"
+                  cy="16"
+                  r="15.5"
+                  stroke={
+                    numOfToken >=
+                    (remainingNFT !== null && remainingNFT < 5
+                      ? remainingNFT
+                      : 5)
+                      ? "#696969"
+                      : "white"
+                  }
+                />
+                <path
+                  d="M15.25 16.75H9.5V15.25H15.25V9.5H16.7499V15.25H22.5V16.75H16.7499V22.5H15.25V16.75Z"
+                  fill={
+                    numOfToken >=
+                    (remainingNFT !== null && remainingNFT < 5
+                      ? remainingNFT
+                      : 5)
+                      ? "#696969"
+                      : "white"
+                  }
+                />
+              </svg>
+            </button>
+          </div>
         ) : (
           <div className="h-12" /> // empty space when not showing input
         )}
@@ -78,7 +176,7 @@ export default function Form() {
             </Button>
           </div>
         )}
-        {/* Mint NFT Button */}
+        {/* TODO: Mint NFT Button */}
         {client.isConnected && remainingNFT !== null && remainingNFT > 0 ? (
           <div className="mb-6 w-full">
             <Button
