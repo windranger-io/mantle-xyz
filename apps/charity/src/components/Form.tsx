70d3950e4e3b90ede4ce94597f2fbea72090f339
Abby L <yu950713@gmail.com>
2023-09-01 13:23:50 +0800

    feat(charity): claim nft error handling

--- apps/charity/src/components/Form.tsx
+++ apps/charity/src/components/Form.tsx
@@ -2,6 +2,7 @@
 
 import { useContext, useEffect, useState } from "react";
 import { useContractWrite } from "wagmi";
+import { BigNumber } from "ethers";
 
 import { Button, Typography } from "@mantle/ui";
 import {
@@ -12,12 +13,12 @@ import {
 import { useTotalMinted } from "@hooks/web3/read";
 import StateContext from "@providers/stateContext";
 import useCurrentClaimPhase from "@hooks/web3/read/useCurrentClaimPhase";
-import { BigNumber } from "ethers";
 
 export default function Form() {
   const { client, setWalletModalOpen } = useContext(StateContext);
 
-  const { isFetchingTotalMinted, totalMinted } = useTotalMinted(L1_NFT_ADDRESS);
+  const { isFetchingTotalMinted, totalMinted, resetTotalMinted } =
+    useTotalMinted(L1_NFT_ADDRESS);
   const [remainingNFT, setRemainingNFT] = useState<number | null>(null);
 
   useEffect(() => {
@@ -47,9 +48,10 @@ export default function Form() {
   const hasActivePhase =
     !isFetchingActiveClaimConditionId && activeClaimConditionId !== undefined;
 
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   // claim NFT
   const {
-    // isLoading: isClaimLoading,
+    isLoading: isClaimLoading,
     // isSuccess: isClaimSuccess,
     write: claim,
   } = useContractWrite({
@@ -58,15 +60,28 @@ export default function Form() {
     functionName: "claim",
     // onSuccess(data) {
     // console.log("Claim Success", data);
+    // TODO: pending page
+    // TODO: wait till confirmation - success
     // },
-    // onError(error) {
-    // console.log("Claim Error", error);
-    // },
+    onError(error) {
+      if (error.toString().includes("!Tokens")) {
+        setErrorMsg(
+          "Invalid mint amount: exceeds available NFTs to mint, please refresh the page and try again."
+        );
+        resetTotalMinted();
+      } else if (error.toString().includes("User rejected the request.")) {
+        setErrorMsg(""); // don't show error msg since this is user decision
+      } else {
+        setErrorMsg(
+          "Something went wrong, please refresh the page and try again."
+        );
+      }
+    },
   });
 
   return (
     <div className="grow sm:my-16 my-10">
-      <div className="sm:w-[444px] w-[320px] flex flex-col items-center gap-4 pt-10 pb-5 sm:px-[62px] px-3 rounded-[40px] border border-boxes-containerStroke bg-boxes-containerBg">
+      <div className="sm:w-[444px] w-[320px] flex flex-col items-center gap-4 pt-10 pb-11 sm:px-[62px] px-3 rounded-[40px] border border-boxes-containerStroke bg-boxes-containerBg">
         <Typography variant="microBody14">Available NFTs to Mint</Typography>
         {/* desktop title */}
         <Typography
@@ -194,7 +209,7 @@ export default function Form() {
         </div>
         {/* Connect Wallet Button */}
         {!client.isConnected && remainingNFT !== 0 && (
-          <div className="mb-6 w-full">
+          <div className="w-full">
             <Button
               type="button"
               size="full"
@@ -210,7 +225,7 @@ export default function Form() {
         currentCondition &&
         remainingNFT !== null &&
         remainingNFT > 0 ? (
-          <div className="mb-6 w-full">
+          <div className="w-full">
             <Button
               type="button"
               size="full"
@@ -245,27 +260,60 @@ export default function Form() {
                 });
               }}
             >
-              {/* eslint-disable-next-line no-nested-ternary */}
-              {!hasActivePhase
-                ? "No active claim phase"
-                : Number(numOfToken) >
-                    (remainingNFT !== null && remainingNFT < 5
-                      ? remainingNFT
-                      : 5) || Number(numOfToken) < 1
-                ? "Invalid mint amount"
-                : "Mint NFT"}
+              <div className="flex flex-row gap-4 items-center mx-auto w-fit">
+                <span>
+                  {/* eslint-disable-next-line no-nested-ternary */}
+                  {!hasActivePhase
+                    ? "No active claim phase"
+                    : Number(numOfToken) >
+                        (remainingNFT !== null && remainingNFT < 5
+                          ? remainingNFT
+                          : 5) || Number(numOfToken) < 1
+                    ? "Invalid mint amount"
+                    : "Mint NFT"}
+                </span>
+                {isClaimLoading ? (
+                  <div role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </Button>
           </div>
         ) : null}
 
         {/* Fully Minted Button */}
         {remainingNFT === 0 ? (
-          <div className="mb-6 w-full">
+          <div className="w-full">
             <Button type="button" size="full" variant="secondary" disabled>
               NFT Collection Fully Minted
             </Button>
           </div>
         ) : null}
+
+        {errorMsg && (
+          <Typography variant="smallWidget16" className="text-type-error">
+            {errorMsg}
+          </Typography>
+        )}
       </div>
     </div>
   );
