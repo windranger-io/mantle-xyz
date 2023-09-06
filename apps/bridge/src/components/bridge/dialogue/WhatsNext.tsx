6ac98948c424dba5de7809eaecd7b558c9ad0535
Abby L <yu950713@gmail.com>
2023-09-06 14:55:44 +0800

    feat(bridge): remove cta btn, add close btn, change last redirect url to MJ in what's next screen

--- apps/bridge/src/components/bridge/dialogue/WhatsNext.tsx
+++ apps/bridge/src/components/bridge/dialogue/WhatsNext.tsx
@@ -1,13 +1,17 @@
-import { MouseEventHandler, useContext } from "react";
+import { MouseEventHandler } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import CONST from "@mantle/constants";
 import { Typography } from "@mantle/ui";
 
-import StateContext from "@providers/stateContext";
-import { DELEGATION_URL, MANTLE_BRIDGE_URL } from "@config/constants";
-import AddNetworkBtn from "@components/bridge/dialogue/AddNetworkBtn";
+// import StateContext from "@providers/stateContext";
+import {
+  DELEGATION_URL,
+  // MANTLE_BRIDGE_URL,
+  MANTLE_JOURNEY_URL,
+} from "@config/constants";
+// import AddNetworkBtn from "@components/bridge/dialogue/AddNetworkBtn";
 
 function WhatNextLink({
   href,
@@ -32,8 +36,8 @@ function WhatNextLink({
       onClick={(ev) => onClick?.(ev)}
     >
       <div className="flex items-center justify-between mb-4">
-        <div className="flex items-center gap-4">
-          <div className="relative h-[100px] md:w-[180px] w-[140px] rounded-lg border border-[#1C1E20] bg-black">
+        <div className="flex items-center gap-4 max-w-[95%] mr-2">
+          <div className="relative h-[100px] md:min-w-[180px] min-w-[120px] rounded-lg border border-[#1C1E20] bg-black">
             <Image
               fill
               src={image}
@@ -43,7 +47,7 @@ function WhatNextLink({
           </div>
           <div className="flex flex-col">
             <Typography variant="modalHeadingSm">{title}</Typography>
-            <Typography>{description}</Typography>
+            <Typography className="hidden sm:block">{description}</Typography>
           </div>
         </div>
         {/* chevron right */}
@@ -67,12 +71,31 @@ function WhatNextLink({
 }
 
 export default function WhatsNext({ closeModal }: { closeModal: () => void }) {
-  const { chainId } = useContext(StateContext);
+  // const { chainId } = useContext(StateContext);
 
   return (
-    <>
-      <span className="flex justify-between align-middle">
-        <Typography variant="modalHeading" className="text-center w-full mt-4">
+    <div className="relative">
+      <button
+        type="button"
+        className="absolute right-0 top-0"
+        onClick={closeModal}
+      >
+        <span className="sr-only">Close menu</span>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.5 14.1L1.9 24.75C1.66667 24.95 1.4 25.05 1.1 25.05C0.8 25.05 0.533334 24.95 0.3 24.75C0.1 24.5167 0 24.25 0 23.95C0 23.65 0.1 23.3833 0.3 23.15L10.95 12.5L0.35 1.9C0.116667 1.7 0 1.45 0 1.15C0 0.850001 0.1 0.583334 0.3 0.35C0.533334 0.116667 0.8 0 1.1 0C1.4 0 1.66667 0.116667 1.9 0.35L12.5 11L23.1 0.35C23.3333 0.15 23.6 0.0500002 23.9 0.0500002C24.2 0.0500002 24.4667 0.15 24.7 0.35C24.9 0.583334 25 0.850001 25 1.15C25 1.45 24.9 1.71667 24.7 1.95L14.05 12.55L24.7 23.2C24.9 23.4 25 23.65 25 23.95C25 24.25 24.9 24.5 24.7 24.7C24.4667 24.9333 24.2083 25.05 23.925 25.05C23.6417 25.05 23.3833 24.9333 23.15 24.7L12.5 14.1Z"
+            fill="white"
+          />
+        </svg>
+      </button>
+      <span className="flex justify-between align-middle mb-8">
+        <Typography variant="modalHeading" className="text-center w-full">
           What to do next?
         </Typography>
       </span>
@@ -93,6 +116,13 @@ export default function WhatsNext({ closeModal }: { closeModal: () => void }) {
         />
         <WhatNextLink
           image="/deposited/bridge.png"
+          title="Mantle Journey"
+          description="Claim your unique Mantle identity and explore performance-based rewards"
+          href={MANTLE_JOURNEY_URL || "#"}
+          newTab
+        />
+        {/* <WhatNextLink
+          image="/deposited/bridge.png"
           title="Bridge"
           description="Move your tokens to Mantle Network"
           href={MANTLE_BRIDGE_URL[chainId] || "#"}
@@ -101,11 +131,11 @@ export default function WhatsNext({ closeModal }: { closeModal: () => void }) {
             // this link will be ignored (but cmd+clicks will work) - manually close the modal and reset
             closeModal();
           }}
-        />
+        /> */}
       </div>
-      <div>
+      {/* <div>
         <AddNetworkBtn />
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 }
