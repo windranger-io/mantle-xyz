48e226650a55ba45298f921a7126fc2ea0bed796
Will Demaine <willdemaine@gmail.com>
2023-09-06 20:08:51 +0300

    feat(lsp): scaffold LSP app

---
+++ apps/lsp/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...
+  presets: [require('@mantle/tailwind-config')],
+
+  // Project-specific customizations
+  theme: {
+    //...
+  },
+}
+
+// const config = require('@mantle/tailwind-config')
+
+// module.exports = config
