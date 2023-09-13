7cb65187ddbc7813d475333705e03c1675410262
grezle <4310551+grezle@users.noreply.github.com>
2023-09-13 20:37:07 +0100

    feat(converter): implement migratorV2 supagraph

--- apps/converter/src/supagraph/handlers/index.ts
+++ apps/converter/src/supagraph/handlers/index.ts
@@ -1,14 +1,28 @@
-// type the handlers
+// Type the handlers
 import { Handlers } from "supagraph";
 
 // Import the handlers
+import { ApprovalHandler } from "./approvals";
 import { TokensMigratedHandler } from "./migrator";
+import {
+  TokensMigratedV2Handler,
+  TokenMigrationApprovedV2Handler,
+} from "./migratorv2";
 
 // Construct the handlers to register each contract against its handlers
 export const handlers: Handlers = {
-  // construct as a named group
+  // construct as a named groups
+  token: {
+    // eventName -> handler()
+    Approval: ApprovalHandler,
+  },
   migrator: {
     // eventName -> handler()
     TokensMigrated: TokensMigratedHandler,
+  },
+  migratorV2: {
+    // eventName -> handler()
+    TokensMigrated: TokensMigratedV2Handler,
+    TokenMigrationApproved: TokenMigrationApprovedV2Handler,
   },
 };
---
+++ apps/converter/src/supagraph/handlers/migratorv2.ts
@@ -0,0 +1,102 @@
+// We use BigNumber to handle all numeric operations
+import { BigNumber } from "ethers";
+
+// Use Store to interact with entity storage
+import { Store } from "supagraph";
+
+// Each event is supplied the block and tx along with the typed args
+import {
+  Block,
+  TransactionReceipt,
+  TransactionResponse,
+} from "@ethersproject/providers";
+
+// - These types will be generated based on the event signatures exported by the defined contracts in config (coming soon TM);
+import type {
+  AccountEntity,
+  MigationEntity,
+  TokenMigrationApprovedEvent,
+  TokensMigratedEvent,
+} from "../types";
+
+// Generic handler to consume TokensMigrated events (event TokensMigrated(address indexed to, uint256 amountSwapped))
+export const TokensMigratedV2Handler = async (
+  args: TokensMigratedEvent,
+  { tx, block }: { tx: TransactionReceipt & TransactionResponse; block: Block }
+) => {
+  // load the entity for this account and migration (migration based on txHash)ç
+  const account = await Store.get<AccountEntity>("Account", args.to);
+  const migration = await Store.get<MigationEntity>(
+    "PendingMigrationV2",
+    args.to
+  );
+
+  // add the migration to users migratedMnt total
+  const newBalance = BigNumber.from(account.migratedMnt || "0").add(
+    args.amountSwapped
+  );
+  // keep count of how many migrations the user has made (so we can paginate Migrations)
+  const newCount = BigNumber.from(account.migrationCount || "0").add("1");
+
+  // update/set the accounts details
+  account.set("migratedMnt", newBalance);
+  account.set("migrationCount", newCount);
+  account.set("blockNumber", tx.blockNumber);
+  account.set("transactionHash", tx.transactionHash);
+
+  // add a migration for each event (recording all relevant details from the args, tx and block)
+  migration.set("account", args.to);
+  migration.set(
+    "amountApproved",
+    // if an approver approves themselves via the MigratorV2 contract, the Approval event on the BIT contract will be for 0
+    BigNumber.from(migration.amountApproved).eq("0")
+      ? args.amountSwapped
+      : migration.amountApproved
+  );
+  migration.set("amountSwapped", args.amountSwapped);
+  migration.set("blockNumber", tx.blockNumber);
+  migration.set("blockTimestamp", block.timestamp);
+  migration.set("transactionHash", tx.transactionHash);
+
+  // set the status to complete
+  migration.set("status", "COMPLETE");
+
+  // store the pending entity (keep this around for easy most recent tx lookup/to simpifly the procedure)
+  const newMigration = await migration.save();
+
+  // store the same entity against its txHash instead of pending into the MigrationV2 collection
+  await Store.set(
+    // ref to the entity table
+    "MigrationV2",
+    // set against hash
+    tx.transactionHash || tx.hash,
+    // pull the full object details
+    {
+      ...newMigration.valueOf(),
+      // copy the correct id into the new entity
+      id: tx.transactionHash || tx.hash
+    }
+  );
+
+  // save all changes on account
+  await account.save();
+};
+
+// Generic handler to consume TokensMigrated events (event TokensMigrated(address indexed to, uint256 amountSwapped))
+export const TokenMigrationApprovedV2Handler = async (
+  args: TokenMigrationApprovedEvent,
+  { tx }: { tx: TransactionReceipt & TransactionResponse }
+) => {
+  // load the entity for this account and migration (migration based on txHash)
+  const migration = await Store.get<MigationEntity>(
+    "PendingMigrationV2",
+    args.user
+  );
+
+  // record gas-cost for future refund
+  migration.set("approvedBy", args.approver);
+  migration.set("approvalTx", tx.transactionHash || tx.hash);
+
+  // save all changes
+  await migration.save();
+};
