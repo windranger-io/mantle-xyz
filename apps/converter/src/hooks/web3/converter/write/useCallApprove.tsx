12bcaf73e61a5ffa59fa9f9a4ea81642cb2a6680
grezle <4310551+grezle@users.noreply.github.com>
2023-09-18 09:33:46 +0100

    fix(converter): remove logs

--- apps/converter/src/hooks/web3/converter/write/useCallApprove.tsx
+++ apps/converter/src/hooks/web3/converter/write/useCallApprove.tsx
@@ -60,7 +60,7 @@ export function useCallApprove() {
       } catch {
         // log the approval was cancelled
         setApprovalStatus("Approval cancelled");
-        console.log("Approval cancelled");
+        // change the page to show error
         setCTAPage(CTAPages.Error);
       } finally {
         // call this to reset the allowance in the ui
