e82335d71ca17740b33485b886a4153e26f6cd48
Will Demaine <willdemaine@gmail.com>
2023-09-20 19:00:07 +0700

    feat(lsp): initial work on stat panel

--- apps/lsp/src/app/layout.tsx
+++ apps/lsp/src/app/layout.tsx
@@ -7,14 +7,15 @@ import {
   PageContainer,
   SlimFooter,
 } from "@mantle/ui";
-
 import Link from "next/link";
+
 import Head from "@app/head";
 import Providers from "@app/providers";
 import Nav from "@components/Nav";
 import CONST from "@mantle/constants";
-
+import StatPanel from "@components/StatPanel";
 import { Faq } from "@components/Faq";
+
 import bridgeBG from "../../public/bridge-bg.png";
 
 export default function RootLayout({
@@ -45,9 +46,7 @@ export default function RootLayout({
               <div className="relative w-full lg:min-w-[484px] lg:w-[484px] flex flex-col md:flex-row lg:block lg:mx-auto ">
                 {children}
                 <div className="flex flex-col mt-4 sm:mt-0 xl:w-[280px] lg:w-[240px] md:max-w-[328px] md:min-w-[250px] lg:absolute lg:top-0 lg:-right-[260px] xl:-right-[396px] w-auto space-y-8">
-                  <div className="max-w-[484px] relative border border-[#1C1E20] mx-auto rounded-xl w-full overflow-x-auto">
-                    <p className="bg-black p-6">Stat panel here</p>
-                  </div>
+                  <StatPanel />
                   <div className="max-w-[484px] relative border border-[#1C1E20] mx-auto rounded-xl w-full overflow-x-auto">
                     <Faq />
                   </div>
