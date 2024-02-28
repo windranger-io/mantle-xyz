8afe675abf398019b26da21969177d05b7c8189d
Albert <albert.zhang@mantle.xyz>
2024-02-28 09:31:06 +0800

    feat: add upgrade page (#193)
    * feat: add maintenance page
    
    * feat: update maintenance

--- apps/bridge/next.config.js
+++ apps/bridge/next.config.js
@@ -1,19 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@design-system/ui'],
+  transpilePackages: ["@design-system/ui"],
   images: {
     domains: [
-      'token-list.mantle.xyz',
-      'safe-transaction-assets.gnosis-safe.io',
-      'safe-transaction-assets.safe.global',
-      'static.alchemyapi.io',
+      "token-list.mantle.xyz",
+      "safe-transaction-assets.gnosis-safe.io",
+      "safe-transaction-assets.safe.global",
+      "static.alchemyapi.io",
     ],
   },
   experimental: {
     appDir: true,
-    scrollRestoration: true
-  }
-}
+    scrollRestoration: true,
+  },
+  async redirects() {
+    const redirectMaintenancePage =
+      process.env.NEXT_PUBLIC_SHOW_MAINTENANCE === "true"
+        ? [
+            {
+              source: "/",
+              destination: "/maintenance",
+              permanent: true,
+            },
+            {
+              source: "/((?!maintenance).*)",
+              destination: "/maintenance",
+              permanent: true,
+            },
+          ]
+        : [
+            {
+              source: "/maintenance",
+              destination: "/",
+              permanent: false,
+            },
+          ];
 
-module.exports = nextConfig
+    return redirectMaintenancePage;
+  },
+};
+
+module.exports = nextConfig;
