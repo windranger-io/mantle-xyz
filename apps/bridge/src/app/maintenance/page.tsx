d09c0fc8ede92f4a1fe69a6454755386b91211e4
Albert <albert.zhang@mantle.xyz>
2024-03-15 09:10:41 +0800

    display maintenance for upgrade mantle v2 (#199)
    * feat: add maintenance page
    
    * feat: update maintenance link
    
    * feat(bridge): update maintenance page

---
+++ apps/bridge/src/app/maintenance/page.tsx
@@ -0,0 +1,22 @@
+import { T } from "@mantle/ui";
+
+const Page = async () => {
+  return (
+    <div className="flex items-center flex-col justify-center mt-10">
+      <T variant="h5Title">Mantle v2 Tectonic Upgrade</T>
+      <T variant="body20" className="mt-10">
+        The network is upgrading, please wait...{" "}
+        <a
+          href="https://twitter.com/0xMantle/status/1767165298724295108"
+          target="__blank"
+          rel="noreferrer"
+          className="underline mt-4"
+        >
+          Learn more
+        </a>
+      </T>
+    </div>
+  );
+};
+
+export default Page;
