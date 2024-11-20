38cc6f53b537d164c30a8097b9632fb2a0244eec
Li Yanfeng <yanfeng.li@mantle.xyz>
2024-11-20 11:17:30 +0800

    feat: update rest rpcUrls

--- apps/faucet/src/app/rpc/route.ts
+++ apps/faucet/src/app/rpc/route.ts
@@ -9,10 +9,12 @@ const fetchInfura = async (
   method: string,
   params: any[]
 ) => {
+  const isGoerli = CHAINS[L1_CHAIN_ID].chainName.toLowerCase() === "goerli";
+
   const res = await fetch(
-    `https://${CHAINS[L1_CHAIN_ID].chainName.toLowerCase()}.infura.io/v3/${
-      process.env.NEXT_PUBLIC_INFURA_API_KEY
-    }`,
+    isGoerli
+      ? "https://goerli.infura.io/v3/927668fc3dec43bcb1225299596c2e58"
+      : "https://eth-mainnet.g.alchemy.com/v2/C1HA_ubz9iHEBkGZi-LxwHijrRHzRhUe",
     {
       method: "post",
       headers: {
