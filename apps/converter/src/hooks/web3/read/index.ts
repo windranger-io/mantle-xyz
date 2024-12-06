3eab585afc1fe2a6b62f1f1eb1d649752915f24c
arjen <arjen.chen@mantle.xyz>
2024-12-06 16:15:10 +0800

    fix: remove fee data

--- apps/converter/src/hooks/web3/read/index.ts
+++ apps/converter/src/hooks/web3/read/index.ts
@@ -1,6 +1,4 @@
 // export everything in the directory
 import useAccountBalances from "./useAccountBalances";
-import useL1FeeData from "./useL1FeeData";
-import useL2FeeData from "./useL2FeeData";
 
-export { useAccountBalances, useL1FeeData, useL2FeeData };
+export { useAccountBalances };
