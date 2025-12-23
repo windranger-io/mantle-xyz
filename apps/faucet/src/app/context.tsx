3f84259e3a5bea7cae9edaeb1962ff132222e635
Luke Long <yin.long@mantle.xyz>
2025-12-23 14:34:49 +0800

    chore: test

--- apps/faucet/src/app/context.tsx
+++ apps/faucet/src/app/context.tsx
@@ -12,7 +12,7 @@ export default function AuthContext({ children, session }: AuthContextProps) {
   // we could filter the session content here to avoid sending any session secrets down the wire
   return (
     <SessionProvider
-      refetchInterval={3_000}
+      // refetchInterval={3_000}
       refetchOnWindowFocus
       session={session}
     >
